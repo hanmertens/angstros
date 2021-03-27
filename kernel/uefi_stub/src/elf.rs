53da4f645a2c2f93d57c5712b6c266a02fefd87e
Han Mertens <hanmertens@outlook.com>
2021-03-27 21:30:24 +0100

    Allow virtual/physical size mismatch in ELF
    This will soon be necessary because of the static variables we want to
    use for our logger. While we're at it, we use information from the ELF
    to select the desired page table flags.
    
    When the virtual length is larger than the physical length, the relevant
    information from the last frame is copied to a fresh frame, which is
    mapped to the desired page. If necessary, additional fresh frames are
    allocated and mapped to subsequent pages. All memory in the newly
    allocated frames beyond the copied data is zeroed out.

--- kernel/uefi_stub/src/elf.rs
+++ kernel/uefi_stub/src/elf.rs
@@ -1,5 +1,6 @@
 //! Helpers for dealing with the kernel ELF.
 
+use core::ptr;
 use x86_64::{
     structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, PhysFrame, Size4KiB},
     PhysAddr, VirtAddr,
@@ -52,7 +53,7 @@ impl<'a> ElfInfo<'a> {
                     self.load_segment(&header, map, all)?;
                 }
                 ty => {
-                    log::debug!("    Skipping section of type {:?}", ty);
+                    log::debug!("Skipping section of type {:?}", ty);
                 }
             }
         }
@@ -75,40 +76,87 @@ impl<'a> ElfInfo<'a> {
         if virt_len == 0 {
             return Ok(());
         }
+        let flags = {
+            let mut flags = PageTableFlags::PRESENT;
+            if header.flags().is_write() {
+                flags |= PageTableFlags::WRITABLE;
+            }
+            if !header.flags().is_execute() {
+                flags |= PageTableFlags::NO_EXECUTE;
+            }
+            flags
+        };
         let virt_start = VirtAddr::new(header.virtual_addr());
-        let virt_end = virt_start + (virt_len - 1);
+        let virt_end = virt_start + virt_len - 1u64;
         let phys_start =
             PhysAddr::new(VirtAddr::from_ptr(self.0.input as *const _ as *const u8).as_u64())
                 + header.offset();
-        let phys_end = phys_start + (phys_len - 1);
-        if virt_len == phys_len {
-            log::debug!(
-                "    Mapping {:?}..{:?} to {:?}..{:?}",
-                virt_start,
-                virt_end,
-                phys_start,
-                phys_end
-            );
-            for (page, frame) in Page::range_inclusive(
-                Page::containing_address(virt_start),
-                Page::containing_address(virt_end),
-            )
-            .zip(PhysFrame::range_inclusive(
-                PhysFrame::containing_address(phys_start),
-                PhysFrame::containing_address(phys_end),
-            )) {
-                log::debug!("        Mapping {:?} to {:?}", page, frame);
-                let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+        let phys_end = phys_start + phys_len - 1u64;
+        log::debug!(
+            "Mapping {:?}..{:?} to {:?}..{:?}",
+            virt_start,
+            virt_end,
+            phys_start,
+            phys_end
+        );
+        let mut page_range = Page::range_inclusive(
+            Page::containing_address(virt_start),
+            Page::containing_address(virt_end),
+        );
+        let frame_range = PhysFrame::range_inclusive(
+            PhysFrame::containing_address(phys_start),
+            PhysFrame::containing_address(phys_end),
+        );
+        if virt_len > phys_len {
+            // Instead of mapping to the last ELF frame, map to fresh frame
+            // Other extraneous virtual memory is also backed by fresh frames
+            let new_start = Page::containing_address(virt_start + phys_len - 1u64);
+            let old_end = page_range.end;
+            page_range.end = new_start - 1;
+            let new_range = Page::range_inclusive(new_start, old_end);
+            for (i, page) in new_range.enumerate() {
+                let frame = all.allocate_frame().ok_or("No frame allocated")?;
+                log::trace!("Mapping {:?} to fresh {:?}", page, frame);
                 unsafe { map.map_to(page, frame, flags, all) }
                     .map_err(|e| {
                         log::error!("{:?}", e);
                         "Mapping error"
                     })?
                     .ignore();
+                // Copy data from ELF to first fresh frame
+                let zero_start = if i == 0 {
+                    let phys_start = phys_start.max(frame_range.end.start_address());
+                    let offset = phys_start - phys_start.align_down(4096u64);
+                    let count = phys_end - phys_start + 1;
+                    let fresh_start = frame.start_address() + offset;
+                    log::trace!(
+                        "Copying {} bytes from {:?} to {:?}",
+                        count,
+                        phys_start,
+                        fresh_start,
+                    );
+                    let src = phys_start.as_u64() as *const u8;
+                    let dst = fresh_start.as_u64() as *mut u8;
+                    unsafe { ptr::copy_nonoverlapping(src, dst, count as usize) };
+                    offset + count
+                } else {
+                    0
+                };
+                // Zero memory using current identity mapping
+                let frame_ptr = (frame.start_address().as_u64() + zero_start) as *mut u8;
+                unsafe { ptr::write_bytes(frame_ptr, 0, 4096 - zero_start as usize) };
             }
-            Ok(())
-        } else {
-            Err("Virtual/physical segment length mismatch")
         }
+        // Map directly to ELF as loaded in static variable
+        for (page, frame) in page_range.zip(frame_range) {
+            log::trace!("Mapping {:?} to {:?}", page, frame);
+            unsafe { map.map_to(page, frame, flags, all) }
+                .map_err(|e| {
+                    log::error!("{:?}", e);
+                    "Mapping error"
+                })?
+                .ignore();
+        }
+        Ok(())
     }
 }
